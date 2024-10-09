6ad8fc7dab45675fad13ae5bd30f9ef3759e0d2a
Javier Delgado Chacón <jdelgadoc17@iesagora.caceres>
2024-10-09 18:05:26 +0200

    first commit

---
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.calculadora;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.calculadora.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+    }
+}
